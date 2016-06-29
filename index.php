57fd33008411565477486cd2233820de4a24c50e
mahbubme <mail@mahbub.me>
2016-06-29 22:09:14 +0600

    created frontpage layout

--- index.php
+++ index.php
@@ -18,53 +18,124 @@
 		<![endif]-->
 	</head>
 	<body>
-
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Title</a>
-				</div>
 		
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Link</a></li>
-						<li><a href="#">Link</a></li>
-					</ul>
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
+		<header id="header">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container">
+					<div class="row">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Social Network</a>
 						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Link</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Action</a></li>
-								<li><a href="#">Another action</a></li>
-								<li><a href="#">Something else here</a></li>
-								<li><a href="#">Separated link</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div>
-		</nav>
+				
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse navbar-ex1-collapse">
+							<form action="" method="post" class="navbar-form navbar-right" role="search">
+								<div class="form-group">
+									<label for="email">Email:</label>
+									<input type="email" name="email" class="form-control" placeholder="Email" required>
+								</div>
+								<div class="form-group">
+									<label for="pass">Password:</label>
+									<input type="password" name="pass" class="form-control" placeholder="*****" required>
+								</div>
+								<button type="submit" name="sub" class="btn btn-default">Login</button>
+							</form>
+						</div><!-- /.navbar-collapse -->
+					</div>
+				</div>
+			</nav>
+		</header>
 
-		<h1 class="text-center">Hello World</h1>
+		<section id="content">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-7 text-center">
+						<h1>Join the Largest Social Network</h1>
+						<img src="assets/images/1.png" class="img-responsive inline-block" alt="">
+					</div>
+					<div class="col-sm-5">
+						<form method="post" class="form-horizontal">
+							<div class="form-group">
+								<div class="col-sm-offset-2 col-sm-10">
+									<h3 class="text-center">Sign Up Here</h3>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="u_name" class="col-sm-2">Name:</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" name="u_name" placeholder="Enter your name" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="u_password" class="col-sm-2">Password:</label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" name="u_password" placeholder="Enter your password" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="u_email" class="col-sm-2">Email:</label>
+								<div class="col-sm-10">
+									<input type="email" class="form-control" name="u_email" placeholder="Enter your mail" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="u_country" class="col-sm-2">Country:</label>
+								<div class="col-sm-10">
+									<select class="form-control">
+									  <option>Select a Country</option>
+									  <option>Australia</option>
+									  <option>Bangladesh</option>
+									  <option>United States</option>
+									  <option>United Kingdom</option>
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="u_gender" class="col-sm-2">Gender:</label>
+								<div class="col-sm-10">
+									<select class="form-control">
+									  <option>Select a Gender</option>
+									  <option>Male</option>
+									  <option>Female</option>
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="u_birthday" class="col-sm-2">Birthday:</label>
+								<div class="col-sm-10">
+									<input type="date" class="form-control" name="u_birthday" placeholder="mm/dd/yyyy" required>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-offset-2 col-sm-10">
+									<button type="submit" name="u_submit" class="btn btn-default">Sign Up</button>
+								</div>
+							</div>
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer id="footer">
+			<div class="container">
+				<div class="row">
+					<p class="text-center">© 2016 - www.mahbub.me</p>
+				</div>
+     		 </div>
+		</footer>
 
 		<!-- jQuery -->
 		<script src="assets/js/jquery.js"></script>
 		<!-- Bootstrap JavaScript -->
-		<script src="assets/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+		<script src="assets/sass/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
 	</body>
 </html>
